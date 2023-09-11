ffd01733e5a08e5fc0bd96b19eb4ec239b7f5e7d
lucasrenandns <lucasrenandns96@gmail.com>
2023-09-10 21:45:02 -0300

    Dentist Website

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".team-container", {
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});

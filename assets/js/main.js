4942742bd4964200af9a207098fb7500b812e4b8
lucasrenandns <lucasrenandns96@gmail.com>
2023-09-11 21:06:41 -0300

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+/* ===== Strict mode ===== */
+"use strict"
+
+/* ===== Responsive menu ===== */
+const menuIcon = document.getElementById("menu-icon")
+const navBar = document.getElementById("navbar")
+
+menuIcon.addEventListener("click", function() {
+    navBar.classList.toggle("open")
+    this.classList.toggle("fa-times")
+})
+
+window.addEventListener("scroll", function() {
+    navBar.classList.remove("open")
+    menuIcon.classList.remove("fa-times")
+})
+
+/* ===== Swiper js ===== */
 const swiper = new Swiper(".team-container", {
     grabCursor: true,
     spaceBetween: 20,
